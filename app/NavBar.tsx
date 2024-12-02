d6a8757e7f0d488374fc8af9e293e6a7ec45b5e0
Lahiru De Silva <lahirurashmitha00@gmail.com>
2024-12-02 12:59:35 +0530

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import React from 'react'
 import Link from 'next/link'
 import { FaBug } from "react-icons/fa";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
+
 
 const NavBar = () => {
+
+  const currentPath = usePathname();
+  console.log(currentPath);
 
   const links: Array<any> = [
     { lable: "Dashboard", href: "/" },
@@ -14,10 +22,21 @@ const NavBar = () => {
       <Link className='text-2xl' href="/"><FaBug /></Link>
 
       <ul className='flex space-x-6'>
-        { links.map(link => <Link key={ link.href } className='text-zinc-500 hover:text-zinc-800 transition-colors text-2xl' href={ link.href } > { link.lable } </Link>) }
+        { 
+          links.map(link => <Link key={ link.href } 
+          // Same thing doing using Classname pakage (npm install classnames)
+          // className={ `${ link.href === currentPath ? "text-zinc-900" : "text-zinc-500" } hover:text-zinc-800 transition-colors text-2xl` } 
+          className={ classNames({
+            "text-zinc-900": link.href === currentPath,
+            "text-zinc-500": link.href !== currentPath,
+            "hover:text-zinc-800 transition-colors text-2xl": true
+          }) }
+          href={ link.href } > { link.lable } </Link>) 
+        }
       </ul>
     </nav>
   )
 }
 
-export default NavBar
+
+export default NavBar;
